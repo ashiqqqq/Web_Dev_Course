004a05e7768a12d4ace5f8b1d2c85d66407b7370
Ashiq <ashiq.inform@gmail.com>
2020-01-17 13:40:11 +0800

    Updates till video 3

--- Week3-C.U.R.D Flask.py
+++ Week3-C.U.R.D Flask.py
@@ -1,1 +1,19 @@
-import math
+# create a To Do list App
+# understand which part of the app is the front end and the back end accordingly.
+# Anvil , Python , Git , Version control, Hosting Online
+
+'''
+# C.U.R.D - all modern apps have this functionality (e.g Insta, Google Docs, Dropbox)
+Create
+Read
+Update
+Delete
+'''
+
+ # Database in the real world
+ Disect Youtube
+ 1. Front end is just what u see visually - play button, volume control , etc.
+ 2. Backend - How to control the links, How does youtube rmb all the info, Hoe does youtube rmb the names of video posted
+
+
+# potentially need to use excel sheet ?
